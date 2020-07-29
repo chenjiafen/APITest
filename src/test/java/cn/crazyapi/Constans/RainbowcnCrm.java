d526c78acfa3db9a72a5c93bb6b173c01aa66ad2
tyler <m_chenjiafeng@163.com>
2020-07-29 18:34:07 +0800

    测试请求地址

---
+++ src/test/java/cn/crazyapi/Constans/RainbowcnCrm.java
@@ -0,0 +1,19 @@
+package cn.crazyapi.Constans;
+
+/**
+ * @Author chenjiafneg
+ * @Date 2020/7/28 13:40
+ * @Version 1.0
+ */
+public class RainbowcnCrm {
+    //host
+    public static final  String Base_URL_DIV="dev-hlj.rainbowcn.com";
+    public static final  String Base_URL_SIT="sit-hlj.rainbowcn.com";
+
+    //登录接口
+    public static  final String Rainbowcn_Logind_URL="/api/v1/dubhe-auth/user/th-idm/login";
+
+     //查询所有事件
+    public static  final  String Rainbowcn_EventAll="/api/v1/scrm-member/life-cycle/event/all";
+
+}
